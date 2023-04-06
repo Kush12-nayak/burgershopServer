d53c2c7f74b6a630323bb3a555d13f340924895d
Kush12-nayak <98100356+Kush12-nayak@users.noreply.github.com>
2023-04-06 23:38:52 +0530

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -53,11 +53,12 @@ export const placeOrderOnline=asyncError(
             itemsPrice,
             taxPrice,
             shippingCharges,
-            totalAmount
+            totalAmount,
+            user
         }
         =req.body;
     
-        const user=req.user._id;
+        
     
         const orderOptions={
             shippingInfo,
@@ -67,18 +68,67 @@ export const placeOrderOnline=asyncError(
             taxPrice,
             shippingCharges,
             totalAmount,
-            user
+           user
         }
+
+        const options={
+            amount:Number(totalAmount)*100,
+            currency:"INR",
+        }
+
+        const order=await instance.orders.create(options);
     
-        await Order.create(orderOptions);
     
         res.status(201).json({
             success:true,
-            message:"Order placed successfullt via COD"
+           order,
+           orderOptions
         })
     }
 
 )
+
+export const paymentverification=asyncError(async(req,res,next)=>{
+
+    const {
+        razorpay_payment_id,
+        razorpay_order_id,
+        razorpay_signature,
+        orderOptions
+    }=req.body;
+
+    const body=razorpay_order_id + "|"+ razorpay_payment_id;
+
+    const expectedSignature=crypto.createHmac("sha256",process.env.RAZORPAY_KEY_SECRET)
+    .update(body)
+    .digest("hex")
+
+    const isAuthentic=expectedSignature===razorpay_signature;
+
+
+    if(isAuthentic){
+
+        const payment=await Payment.create({
+            razorpay_payment_id,
+            razorpay_order_id,
+            razorpay_signature,
+        })
+
+        await Order.create({
+            ...orderOptions,
+            paidAt:new Date(Date.now()),
+            paymentInfo:payment._id
+        })
+
+        res.status(201).json({
+            success:true,
+            message:`Payment done successfully. Paymet id:${payment._id}`
+        })
+    }
+    else{
+        return next(new ErrorHandler("Payment Failed",400))
+    }
+})
 
 export const getmyOrders=asyncError(
 
