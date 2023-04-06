527d1a5fe4262fd0671f95b935f924dc4ea09c24
Kush12-nayak <98100356+Kush12-nayak@users.noreply.github.com>
2023-04-06 23:47:52 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -3,12 +3,15 @@ import { isAuthincated,adminauthantication } from "../middlewears/auth.js";
 import { getAdminorders, placeOrder, processOrder,placeOrderOnline } from "../controllers/order.js";
 import {getmyOrders} from "../controllers/order.js"
 import {getOrderDeatails} from "../controllers/order.js"
+import {paymentverification} from "../controllers/order.js"
 
 const router=express.Router();
 
 router.post("/createorder",isAuthincated,placeOrder)
 
 router.post("/createorderonline",isAuthincated,placeOrderOnline)
+
+router.post("/paymentverification",isAuthincated,paymentverification)
 
 router.get("/myorders",isAuthincated,getmyOrders)
 
